2ccf97eea1cf6f8b00ac86d804d3e91e5ff97aeb
Weebadu-arachchige-pathum-Vimukthi <120662772+Weebadu-arachchige-pathum-Vimukthi@users.noreply.github.com>
2024-12-05 07:57:09 +0530

    Commit 01
    Dev mode adding student and removing

---
+++ AddStudent.cs
@@ -0,0 +1,15 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Xml.Linq;
+
+//namespace DSA_Project___Dev_Mode
+//{
+//    public class AddStudent : Developer
+//    {
+//        students.Add(new Student(id, name, grade));
+//    }
+//}
---
+++ Bubble_sort.cs
@@ -0,0 +1,14 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace DSA_Project___Dev_Mode
+//{
+//    public class Bubble_sort : Developer
+//    {
+//        public
+
+//    }
+//}
